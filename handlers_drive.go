50b6c689bef449de5baac6326121af557382e757
kanmuri1206 <65644899+kanmuri1206@users.noreply.github.com>
2021-04-30 15:34:17 +0800

    Modify ClientId & ClientSecret

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "986011441343-186trkq04rntja0p30mf31fjre1n1508.apps.googleusercontent.com"
+const ClientSecret = "9TEjmbV-EZLkkrKaQjnMF5E2"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
